139b7e9845efde8d63b1b7c03fd6c85d184993be
Jasmine Mills <jazzrmills@gmail.com>
2022-04-23 18:32:11 +0100

    add locations

--- controllers/locations_controller.py
+++ controllers/locations_controller.py
@@ -8,9 +8,26 @@ import repositories.city_repository as city_repository
 
 locations_blueprint = Blueprint("locations", __name__)
 
-# New
+# LOCATIONS LIST
 
 @locations_blueprint.route("/locations")
-def locations ():
+def locations():
     locations = location_repository.select_all()
     return render_template("locations/index.html", locations = locations)
+
+# NEW LOCATION
+
+@locations_blueprint.route("/locations/new")
+def new_location():
+    return render_template("locations/new.html")
+
+# NEW LOCATION SUBMIT
+
+@locations_blueprint.route("/locations", methods = ['POST'])
+def create_location():
+    name = request.form["name"]
+    type = request.form["type"]
+    info = request.form["info"]
+    new_location = Location(name,type,info)
+    location_repository.save(new_location)
+    return redirect("/locations")
