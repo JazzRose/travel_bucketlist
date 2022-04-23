8168f4060e5c2c5f41122cdbbe12d4ea233c0714
Jasmine Mills <jazzrmills@gmail.com>
2022-04-23 10:51:48 +0100

    created tables

---
+++ db/bucketlist.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS trips;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS cities;
+
+CREATE TABLE users ( 
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    age INT,
+    interests TEXT
+
+);
+
+CREATE TABLE locations (
+    id SERIAL PRIMARY KEY,
+    city_id INT REFERENCES cities(id),
+    type VARCHAR(255),
+    visited BOOLEAN,
+)
+
+CREATE TABLE cities (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (255),
+    info TEXT
+)
+
+CREATE TABLE visits (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    location_id INT REFERENCES locations(id) ON DELETE CASCADE,
+    review TEXT,
+    rating INT,
+    date VARCHAR(255)
+);
