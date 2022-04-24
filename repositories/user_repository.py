4770cd817322f37e74107d233e1a3518be7facf1
Jasmine Mills <jazzrmills@gmail.com>
2022-04-24 14:55:26 +0100

    user repo

--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -7,3 +7,19 @@ def save(user):
     results  = run_sql(sql,values)
     user.id = results[0]['id']
     return user
+
+def select_all():
+    users = []
+    sql = "SELECT * FROM users"
+    results = run_sql(sql)
+    for result in results:
+        user = User(result["useranme"], result ["age"], result ["interests"], result ["id"])
+        users.append(user)
+    return users
+
+def select(id):
+    sql = "SELECT * FROM users WHERE id = %s"
+    values = [id]
+    result = run_sql(sql,values)[0]
+    user = User(result["useranme"], result ["age"], result ["interests"], result ["id"])
+    return user
